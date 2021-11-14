0bd71f85290dda84035ae7d76da947ef8571f038
Than Htut Zaw <zgyi025@gmail.com>
2021-11-14 21:15:37 +0630

    changed into laravel structur

---
+++ resources/views/section/master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Asxox-Ecommerce</title>
+    <link rel="stylesheet" href="{{url('web public/assets/vendor/bootstrap/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <link rel='stylesheet' href='http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css'>
+    <link rel='stylesheet' href='//fonts.googleapis.com/css?family=Droid+Sans' type='text/css'>
+    <link rel="stylesheet" href="{{url('web public/assets/css/style.css')}}">
+
+
+
+</head>
+<body>
+    @yield('content')
+
+</body>
+
+</html>
